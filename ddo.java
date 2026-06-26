6c806f9ab9a93ea27a820f789ea0b8e1fdbf589f
cricketleagueraad-max <cricketleagueraad@gmail.com>
2026-06-26 21:54:32 +0600

    Update ddo.java

--- ddo.java
+++ ddo.java
@@ -11,9 +11,6 @@ Scanner scanner = new Scanner(System.in);
 System.out.print("Enter the target URL: ");
 String targetUrl = scanner.nextLine();
 
-TEXT
-66 lines
-
 
 String[] userAgents = {
     "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:8.0) Gecko/20100101 Firefox/8.0",
